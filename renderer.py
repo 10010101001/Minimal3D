06f13a86140e2bca7320f9157679d4f3b0766e68
10010101001 <44052322+10010101001@users.noreply.github.com>
2019-11-15 10:07:55 +1100

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -2,4 +2,3 @@ from PIL import Image; from time import sleep;import pygame;from math import sin
 for u in range(-180,1):
   for v in range(-180,180):
     t=(v/float(180))*0.785+Angle;x=int(sin(t)*-u+locX);y=int(cos(t)*-u+locY) ;h=disp[x,y];height=(h-float(disp[locX,locY] )/-u*100 + 120);pygame.draw.line(screen, rgb_im.getpixel((x,y)), (180+v,202),(180+v,202-height),1);pygame.display.flip()
-sleep(10)#Tweak locX, locY and Angle (radians) to your liking. You need to paste a heightmap and colourmap (named height.png and colour.png) into the directory containing the .py file.
